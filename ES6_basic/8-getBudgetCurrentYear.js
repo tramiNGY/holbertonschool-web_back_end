46c151035f3d4a1f5bd4dc31f1c952e4c236f3dc
tramiNGY <ngytrami@gmail.com>
2025-03-25 13:20:16 +0100

    Used CurrentYear

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -5,13 +5,10 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {};
-  const prop_income = "income";
-  const prop_gdp = "gdp";
-  const prop_capita = "capita";
 
-  budget[prop_income] = income;
-  budget[prop_gdp] = gdp;
-  budget[prop_capita] = capita;
+  budget[`income-${getCurrentYear()}`] = income;
+  budget[`gdp-${getCurrentYear()}`] = gdp;
+  budget[`capita-${getCurrentYear()}`] = capita;
 
   return budget;
 }
