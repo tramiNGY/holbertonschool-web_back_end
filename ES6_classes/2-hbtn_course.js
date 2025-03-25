602c6493deb6f16353fbaef26b3c794bf662a100
tramiNGY <ngytrami@gmail.com>
2025-03-25 16:34:36 +0100

    getter setter for attributes and throw TypeError

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newname) {
+    if (typeof newname !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newname;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newlength) {
+    if (typeof newlength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newlength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newstudents) {
+    if (!Array.isArray(newstudents)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = newstudents;
+  }
+}
