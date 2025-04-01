5e73266bc7c573fc30af987c45567750e7ef7243
tramiNGY <ngytrami@gmail.com>
2025-04-01 15:17:28 +0200

    Added finally

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => {
-      console.log('Got a response from the API');
       return {
         status: 200,
-        body: success
+        body: 'success'
       };
     })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return new Error();
-    });
+    .catch(() => Error())
+    .finally(() => console.warn('Got a response from the API'));
 }
