5c22b25e9bf4f8ce34f066b59f874a3e471f1083
tramiNGY <ngytrami@gmail.com>
2025-04-01 10:53:58 +0200

    Used startsWith slice and join

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+  const result = [...set]
+    .filter((item) => item.startsWith(startString))
+    .map((item) => item.slice(startString.length))
+    .join('-');
+
+  return result;
+}
