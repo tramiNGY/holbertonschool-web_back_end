a18058f972338e7da6d17fe1fa6fc7e4c3aa4d21
tramiNGY <ngytrami@gmail.com>
2025-04-01 16:07:54 +0200

    Added if

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,9 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photoResult, userResult]) => {
-      console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+      if (photoResult && userResult) {
+        console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+      }
     })
     .catch(() => {
       console.log('Signup system offline');
