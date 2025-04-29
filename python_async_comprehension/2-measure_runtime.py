2aceab553d6c1cb15e0971870d9d71da20b44dce
tramiNGY <ngytrami@gmail.com>
2025-04-29 23:51:16 +0200

    asyncio.gather to run coroutines in parallel

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+write a measure_runtime coroutine that will execute
+async_comprehension four times in parallel using asyncio.gather.
+measure_runtime should measure the total runtime and return it.
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """Run time for four parallel comprehensions"""
+    start_time = time.time()
+    tasks = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*tasks)
+    end_time = time.time()
+    return end_time - start_time
