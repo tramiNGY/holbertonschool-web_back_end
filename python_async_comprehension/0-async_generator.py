5d007df5572461e05b8fca3e9ef9d9c98c4cb465
tramiNGY <ngytrami@gmail.com>
2025-04-30 00:05:29 +0200

    Added longer function docstyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,7 +9,7 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Async Generator"""
+    """Async Generator using yield"""
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
