dd804c0e8ea668d3899de68b39a05e642a631f6f
tramiNGY <ngytrami@gmail.com>
2025-05-04 17:24:48 +0200

    update attribute

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python3
+"""
+changes all topics of a school document based on the name:
+mongo_collection will be the pymongo collection object
+name (string) will be the school name to update
+topics (list of strings) will be the list of topics
+approached in the school
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Change school topics
+    """
+    mongo_collection.update_one(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
